472b28f7af6e20518b85c75fe6d9eb3d93abcb0e
Dharren Santhalingam <k21097488@github.kcl.ac.uk>
2022-10-14 16:20:35 +0100

    used statistics module

--- median.py
+++ median.py
@@ -1,18 +1,12 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+import statistics
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        sorted_numbers = sorted(numbers)
-        size = len(sorted_numbers)
-        if size % 2 == 0:
-            half = int(size / 2)
-            median = (sorted_numbers[half] + sorted_numbers[half - 1]) / 2
-        else:
-            half = (size - 1) / 2
-            median = sorted_numbers[int(half)]
+        median = statistics.median(numbers)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
